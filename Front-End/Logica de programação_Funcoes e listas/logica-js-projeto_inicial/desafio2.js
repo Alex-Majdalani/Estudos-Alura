462139fc9b53e96f955678f8215c946e24d64a52
Alex Majdalani <alexmajdalanimelo@gmail.com>
2025-06-11 21:47:16 -0300

    exercicios

---
+++ Front-End/Logica de programação_Funcoes e listas/logica-js-projeto_inicial/desafio2.js
@@ -0,0 +1,45 @@
+// Crie uma função que calcule o índice de massa corporal (IMC) de uma pessoa, a partir de sua altura, em metros, e peso, em quilogramas, que serão recebidos como parâmetro. imc = peso/altura2
+function calcImc(peso, altura) {
+  let imc = peso / (altura * altura);
+  console.log(`Seu índice IMC é ${imc}`);
+}
+calcImc(82, 1.69);
+
+// Crie uma função que calcule o valor do fatorial de um número passado como parâmetro.
+/*function calcFatorial (fatorial){
+  let fatorial = if ()
+}*/
+
+// Crie uma função que converte um valor em dólar, passado como parâmetro, e retorna o valor equivalente em reais. Para isso, considere a cotação do dólar igual a R$4,80.
+function cotacaoDolar(valorPossuido) {
+  let valorDolarAtual = 4.8;
+  let cotacao = valorPossuido * valorDolarAtual;
+  console.log(cotacao);
+}
+cotacaoDolar(50);
+
+// Crie uma função que mostre na tela a área e o perímetro de uma sala retangular, utilizando altura e largura que serão dadas como parâmetro.
+function areaPerimetro(altura, largura) {
+  let calcArea = altura * largura;
+  let calcPerimetro = 2 * altura + 2 * largura;
+  console.log(`A área é de ${calcArea} e o perímetro é de ${calcPerimetro}`);
+}
+areaPerimetro(10, 20);
+
+// Crie uma função que mostre na tela a área e o perímetro de uma sala circular, utilizando seu raio que será fornecido como parâmetro. Considere Pi = 3,14. (areaCirc = Pi.r2) (perimetroCir = 2.Pi.r)
+function calcCirc(raio) {
+  let pi = 3.14;
+  let areaCirc = pi * (raio * raio);
+  let perimetroCirc = 2 * pi * raio;
+  console.log(`A área é ${areaCirc} e o perímetro é ${perimetroCirc}`);
+}
+calcCirc(5);
+
+//Crie uma função que mostre na tela a tabuada de um número dado como parâmetro.
+function tabuadaDo(numero) {
+  let n = numero;
+  for (let i = 1; i <= 10; i++) {
+    console.log(`${i} X ${n} = ${i * n}`);
+  }
+}
+tabuadaDo(15);
