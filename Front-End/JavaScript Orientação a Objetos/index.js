e6299f26c89f29a4e205f3f8c270334255fe649a
Alex Majdalani <alexmajdalanimelo@gmail.com>
2025-06-17 20:41:43 -0300

    classes e metodos

--- Front-End/JavaScript Orientação a Objetos/index.js
+++ Front-End/JavaScript Orientação a Objetos/index.js
@@ -1,21 +1,43 @@
 class Cliente {
   nome;
   cpf;
+}
+
+class ContaCorrente {
   agencia;
   saldo;
+
+  depositar(valor) {
+    this.saldo += valor;
+  }
+
+  sacar(valor) {
+    if (this.saldo >= valor) {
+      this.saldo -= valor;
+    } else {
+      console.log(`Valor incompatível com o saldo em conta.`);
+    }
+  }
 }
 
 const cliente1 = new Cliente();
-const cliente2 = new Cliente();
-
 cliente1.nome = "Ricardo";
 cliente1.cpf = 12325474596;
-cliente1.agencia = 1001;
-cliente1.saldo = 0;
 
+const cliente2 = new Cliente();
 cliente2.nome = "Alice";
 cliente2.cpf = 12325474585;
-cliente2.agencia = 1001;
-cliente2.saldo = 0;
 
-console.log(cliente1, cliente2);
+const contaCorrenteRicardo = new ContaCorrente();
+contaCorrenteRicardo.saldo = 0;
+contaCorrenteRicardo.agencia = 1001;
+
+console.log(contaCorrenteRicardo.saldo);
+contaCorrenteRicardo.saldo = 100;
+console.log(contaCorrenteRicardo.saldo);
+contaCorrenteRicardo.sacar(50);
+contaCorrenteRicardo.depositar(200);
+
+console.log(contaCorrenteRicardo.saldo);
+console.log(cliente1);
+console.log(cliente2);
